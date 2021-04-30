be18a3516a436c520d42a22f94a5cc7ba926bd61
iou000 <iou000@naver.com>
2021-05-01 02:30:33 +0900

    시작버튼 추가

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const vertical = document.querySelector('.line_vertical');
 const horizontal = document.querySelector('.line_horizontal');
 const targetImg = document.querySelector('.targetImg');
 const tag = document.querySelector('.tag');
+const start = document.querySelector('.startBtn');
+const game007 = document.querySelector('.game007');
 
-
-document.addEventListener('mousemove', (e) => {
+function mousemove() {
+    document.addEventListener('mousemove', (e) => {
     const x = e.clientX
     const y = e.clientY
 
@@ -15,4 +17,11 @@ document.addEventListener('mousemove', (e) => {
     tag.style.left = `${x}px`;
     tag.style.top = `${y}px`;
     tag.innerHTML = `${x}px, ${y}px`;
+});
+}
+
+start.addEventListener('click', () => {
+    start.style.display = 'none';
+    game007.style.display = 'block';
+    mousemove();
 });
